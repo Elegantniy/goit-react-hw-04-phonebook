4d77337057aa4f30ab5d1063132ec284737f540d
Elegantniy <maxim9083@gmail.com>
2023-05-18 00:29:00 +0300

    hook

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,23 +1,40 @@
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
+import React from 'react';
 import css from './Filter.module.css';
 
-export class Filter extends Component {
-  render() {
-    return (
-      <>
-        <input
-          type="text"
-          name="filter"
-          className={css.input}
-          value={this.props.stateFilter}
-          onChange={this.props.renderOnChange}
-          placeholder='Enter name'
-        />
-      </>
-    );
-  }
-}
+export const Filter = ({ stateFilter, renderOnChange }) => {
+  return (
+    <>
+      <input
+        type="text"
+        name="filter"
+        className={css.input}
+        value={stateFilter}
+        onChange={renderOnChange}
+        placeholder="Enter name"
+      />
+    </>
+  );
+};
+
+
+// Те що було раніше, до Хуків //
+// export class Filter extends Component {
+//   render() {
+//     return (
+//       <>
+//         <input
+//           type="text"
+//           name="filter"
+//           className={css.input}
+//           value={this.props.stateFilter}
+//           onChange={this.props.renderOnChange}
+//           placeholder='Enter name'
+//         />
+//       </>
+//     );
+//   }
+// }
 
 Filter.propTypes = {
   stateFilter: PropTypes.string,
